a58ebaed860e6bd3037cdfc59e84889e71d36fe3
adamszanto <51482667+adamszanto@users.noreply.github.com>
2021-02-12 21:23:13 +0100

    Create linked_list.cpp

---
+++ linked_list.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct node
+{
+    int key_value;
+    node *p_left;
+    node *p_right;
+};
+
+node* insert(node* p_tree, int key)
+{
+    if(p_tree == NULL)
+    {
+        node* p_new_tree = new node;
+        p_new_tree->p_left = NULL;
+        p_new_tree->p_right = NULL;
+        p_new_tree->key_value = key;
+        return p_new_tree;
+    }
+    
+    if(key < p_tree->key_value)
+    {
+        p_tree->p_left = insert(p_tree->p_left, key);
+    }
+    else 
+    {
+        p_tree->p_right = insert(p_tree->p_right, key);
+    }
+    return p_tree;
+}
+
+int main()
+{
+    
+}
